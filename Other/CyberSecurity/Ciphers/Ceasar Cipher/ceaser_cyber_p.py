a462ceb372a5a900342a875b9dfc4f83c9a36e62
jzunic-zoomba <zunicjosh@gmail.com>
2025-07-28 10:32:22 -0400

    Update ceaser_cyber_p.py

--- Other/CyberSecurity/Ciphers/Ceasar Cipher/ceaser_cyber_p.py
+++ Other/CyberSecurity/Ciphers/Ceasar Cipher/ceaser_cyber_p.py
@@ -1,1 +1,29 @@
+#Menue
+# -> Encode/Decode
+#    ->Encode
+#          -> Steps and direction
+#          -> Enter and encode
+#    ->Decode
+#          -> Enter steps and direction
+#          -> Press start
+#          -> Try again?
+# -> Download?
+#     ->Yes/No
+#          -> Yes
+#               -> Download on computer
+# -> Ask for another cyber
+#      -> Yes
+#             ->Repeat
+#      ->No
+#             -End
+
+
+
+######################################################################################################
+
+#Imports
+
+#GUI
+
+#Functions
 
